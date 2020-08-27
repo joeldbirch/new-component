6c4693d51632f151794f7d93842d7e3971946f5c
Joel Birch <joeldbirch@gmail.com>
2020-08-27 11:52:57 +1000

    Fix bug with default options overriding user-specified defaults.

--- src/helpers.js
+++ src/helpers.js
@@ -7,12 +7,12 @@ uninteresting parts of the code, specific to this codebase.
 NOTE: For generalized concerns that aren't specific to this project,
 use `utils.js` instead.
 */
-const os = require('os');
+const os = require('os')
 
-const prettier = require('prettier');
-const chalk = require('chalk');
+const prettier = require('prettier')
+const chalk = require('chalk')
 
-const { requireOptional } = require('./utils');
+const { requireOptional } = require('./utils')
 
 // Get the configuration for this component.
 // Overrides are as follows:
@@ -24,28 +24,25 @@ const { requireOptional } = require('./utils');
 // The CLI args aren't processed here; this config is used when no CLI argument
 // is provided.
 module.exports.getConfig = () => {
-  const home = os.homedir();
-  const currentPath = process.cwd();
+  const home = os.homedir()
+  const currentPath = process.cwd()
 
   const defaults = {
     type: 'functional',
     dir: 'src/components',
     extension: 'js',
-  };
+  }
 
-  const globalOverrides = requireOptional(
-    `/${home}/.new-component-config.json`
-  );
+  const globalOverrides = requireOptional(`/${home}/.new-component-config.json`)
 
   const localOverrides = requireOptional(
     `/${currentPath}/.new-component-config.json`
-  );
-
-  return Object.assign({}, globalOverrides, localOverrides, defaults);
-};
+  )
+  return Object.assign({}, defaults, globalOverrides, localOverrides)
+}
 
 module.exports.buildPrettifier = (prettierConfig) => (text) =>
-  prettier.format(text, prettierConfig);
+  prettier.format(text, prettierConfig)
 
 // Emit a message confirming the creation of the component
 const colors = {
@@ -55,7 +52,7 @@ const colors = {
   gold: [255, 204, 0],
   mediumGray: [128, 128, 128],
   darkGray: [90, 90, 90],
-};
+}
 
 const logComponentType = (selected) =>
   ['class', 'pure-class', 'functional']
@@ -65,44 +62,44 @@ const logComponentType = (selected) =>
         ? `${chalk.bold.rgb(...colors.blue)(option)}`
         : `${chalk.rgb(...colors.darkGray)(option)}`
     )
-    .join('  ');
+    .join('  ')
 
 module.exports.logIntro = ({ name, dir, type }) => {
-  console.info('\n');
+  console.info('\n')
   console.info(
     `✨  Creating the ${chalk.bold.rgb(...colors.gold)(name)} component ✨`
-  );
-  console.info('\n');
+  )
+  console.info('\n')
 
-  const pathString = chalk.bold.rgb(...colors.blue)(dir);
-  const typeString = logComponentType(type);
+  const pathString = chalk.bold.rgb(...colors.blue)(dir)
+  const typeString = logComponentType(type)
 
-  console.info(`Directory:  ${pathString}`);
-  console.info(`Type:       ${typeString}`);
+  console.info(`Directory:  ${pathString}`)
+  console.info(`Type:       ${typeString}`)
   console.info(
     chalk.rgb(...colors.darkGray)('=========================================')
-  );
+  )
 
-  console.info('\n');
-};
+  console.info('\n')
+}
 
 module.exports.logItemCompletion = (successText) => {
-  const checkmark = chalk.rgb(...colors.green)('✓');
-  console.info(`${checkmark} ${successText}`);
-};
+  const checkmark = chalk.rgb(...colors.green)('✓')
+  console.info(`${checkmark} ${successText}`)
+}
 
 module.exports.logConclusion = () => {
-  console.info('\n');
-  console.info(chalk.bold.rgb(...colors.green)('Component created! 🚀 '));
+  console.info('\n')
+  console.info(chalk.bold.rgb(...colors.green)('Component created! 🚀 '))
   console.info(
     chalk.rgb(...colors.mediumGray)('Thanks for using new-component.')
-  );
-  console.info('\n');
-};
+  )
+  console.info('\n')
+}
 
 module.exports.logError = (error) => {
-  console.info('\n');
-  console.info(chalk.bold.rgb(...colors.red)('Error creating component.'));
-  console.info(chalk.rgb(...colors.red)(error));
-  console.info('\n');
-};
+  console.info('\n')
+  console.info(chalk.bold.rgb(...colors.red)('Error creating component.'))
+  console.info(chalk.rgb(...colors.red)(error))
+  console.info('\n')
+}
